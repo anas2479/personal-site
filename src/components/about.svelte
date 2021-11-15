2a9edb806bdc1e297f011145420e7ef8c836e775
Anas Mohamed <amohamed2479@gmail.com>
2021-11-14 23:02:08 -0600

    Create the About section Component

---
+++ src/components/about.svelte
@@ -0,0 +1,11 @@
+<div class="h-screen flex items-center">
+    <div class="md:flex-1">
+        <h2 class="font-headings font-bold text-6xl">
+            Who am I?
+        </h2>
+        <p class="font-body text-3xl">
+            My niche is the Web, but I take a sip of a lot of things that fall under the creator hat. In short, I'm a maker.
+        </p>
+    </div>
+    <div class="md:flex-1"/>
+</div>
