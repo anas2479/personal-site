44ee42a34e40b1efe5d2ac3f12bce1e3adf6500f
Anas Mohamed <amohamed2479@gmail.com>
2021-11-14 23:02:51 -0600

    Create projects store

---
+++ src/stores.js
@@ -0,0 +1,11 @@
+import { writable } from "svelte/store";
+
+
+
+export let projects = writable([
+    {
+        title: "Shell Ninja",
+        description:"A web-based interactive tutorial that is designed to teach beginners the basics of using shell programs",
+        url:"https://anas2479.notion.site/Shell-Ninja-289e633be39544b4a4a65da5a3704b59"
+    }
+])
