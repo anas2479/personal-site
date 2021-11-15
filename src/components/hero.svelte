a5e67d8b1478607bd497339b129acec3ce2280fe
Anas Mohamed <amohamed2479@gmail.com>
2021-11-14 23:01:53 -0600

    Create the Hero Component

---
+++ src/components/hero.svelte
@@ -0,0 +1,21 @@
+<div class="h-screen w-full grid md:grid-cols-3 items-center">
+
+    <div class=" mb-4">
+        <h1 class="font-headings text-8xl font-bold">Hi</h1>
+        <p class="font-body text-3xl">I’m Anas, an aspiring designer/developer.</p>
+    </div>
+    <div class="or order-first md:order-none flex justify-center mb-4">
+        <img src="./assets/face.svg" alt="">
+    </div>
+    <div class=" mb-4">
+        <p class="font-body text-2xl text-gray-700">
+            Currently interning for the Security Technology Operations team @3M.
+        </p>
+    </div>
+
+</div>
+
+
+<script>
+
+</script>
