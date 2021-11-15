ef2532db3cee0b8e834575cdd767a3f1571e4070
Anas Mohamed <amohamed2479@gmail.com>
2021-11-14 23:05:43 -0600

    Put the `Hero`, `About`, and `Projects` components in the Main component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,17 @@
 <div class="max-w-6xl mx-auto p-4">
+    <Hero/>
 
+    <About/>
 
+    <Projects/>
 </div>
 
 <script>
+import About from "./components/about.svelte";
 
+import Hero from "./components/hero.svelte";
 
+import Projects from "./components/projects.svelte";
 
 
 </script>
