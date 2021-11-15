a368ad8b0b363ef00b4be3fc5f84b53b1cb42971
Anas Mohamed <amohamed2479@gmail.com>
2021-11-15 17:58:55 -0600

    add the new illustration to the About component

--- src/components/about.svelte
+++ src/components/about.svelte
@@ -1,11 +1,16 @@
 <div class="h-screen flex items-center">
-    <div class="md:flex-1">
-        <h2 class="font-headings font-bold text-6xl">
-            Who am I?
-        </h2>
-        <p class="font-body text-3xl">
-            My niche is the Web, but I take a sip of a lot of things that fall under the creator hat. In short, I'm a maker.
-        </p>
+    <div class="flex flex-col md:flex-row">
+        <div class="md:flex-1">
+            <h2 class="font-headings font-bold text-6xl">
+                Who am I?
+            </h2>
+            <p class="font-body text-3xl">
+                My niche is the Web, but I take a sip of a lot of things that fall under the creator hat. In short, I'm a maker.
+            </p>
+        </div>
+        <div class="md:flex-1">
+            <img src="./assets/about-me.svg" alt="About Anas">
+        </div>
     </div>
-    <div class="md:flex-1"/>
+    
 </div>
