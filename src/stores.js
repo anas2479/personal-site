a5bfcdcff935bf29d12577afdcaa6cd7a0fba526
Anas Mohamed <49694746+anas2479@users.noreply.github.com>
2022-08-21 19:01:45 -0500

    update projects

--- src/stores.js
+++ src/stores.js
@@ -4,8 +4,19 @@ import { writable } from "svelte/store";
 
 export let projects = writable([
     {
+        title: 'Form Chimp',
+        description: 'A platform that allows you to build web forms without worrying about writing server code.',
+        url:'https://anasweb.notion.site/Form-Chimp-A-Project-By-Anas-Mohamed-3b68631525774aba97786a219f0005ea',
+
+    },
+    {
+        title: `End-Qabyaalad`,
+        description:`Creating social awareness and education campaigns about the disadvantages and backwardness of the patrimonial system of Clannism (Qabyaalad).`,
+        url:`https://anasweb.notion.site/End-Qabyaalad-38929ef4860148878121d18f1c2aaec8`
+    },
+    {
         title: "Shell Ninja",
-        description:"A web-based interactive tutorial that is designed to teach beginners the basics of using shell programs",
-        url:"https://anas2479.notion.site/Shell-Ninja-289e633be39544b4a4a65da5a3704b59"
+        description:"A web-based interactive tutorial that is designed to teach beginners the basics of using shell programs.",
+        url:"https://anasweb.notion.site/Shell-Ninja-1647c30a30ec44d2848738613327a5c1"
     }
 ])
